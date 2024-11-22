17f9faeecd96f59351c003259f524e5839e98dfe
Abishek Elumalai <151460065+AbishekLE@users.noreply.github.com>
2024-11-22 16:20:47 +0530

    Delete Global.js

--- Global.js
+++
@@ -1,6 +0,0 @@
-// JavaScript to trigger fade-in effect on page load
-window.addEventListener('load', function() {
-    const container = document.querySelector('.container');
-    container.style.opacity = 1;
-  });
-  
